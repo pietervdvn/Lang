343d4c6e166f505a7f2e4523501920fde4a2b1f1
pietervdvn <pietervdvn@gmail.com>
2014-06-12 18:08:40 +0200

    More syntax! Typedefs now declared

--- Parser/Test.hs
+++ Parser/Test.hs
@@ -4,20 +4,24 @@ import Bnf
 import Data.Maybe
 import Normalizable
 import Data.Maybe
-
+import Bnf.ParseTree
 {--
 
 This module loads and compiles the bnf's to test them 
 
 --}
 
-t		:: String -> String -> IO ()
-t rule str	=  do	world	<- load "bnf/Languate"
+pt		:: String -> String -> IO ParseTree
+pt rule str	=  do	world	<- load "bnf/Languate"
 			print $ length $ show world	-- force evaluation, as to show exceptions
 			let pt'	= fromJust $ parse world (toFQN ["Languate"]) rule str
 			let pt  = case pt' of
 					Right pt	-> pt
 					Left exception	-> error $ show exception
-			print $ simplify $ normalize pt
+			return $ normalize pt
+
+t rule str	=  pt rule str >>= print . simplify
+
+tr rule str	= pt rule str >>= print
 
 main	= t "lang" "123"
