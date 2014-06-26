388e2700d5da77c94c8d4f8b568ba1fb2f9f3f4d
pietervdvn <pietervdvn@gmail.com>
2014-06-26 16:01:44 +0200

    Scripts

--- installAll.sh
+++ installAll.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
-# installs all subcomponents, in the right order!
+# get latest version
+git pull
 
+# installs all subcomponents, in the right order
 cabal install Consumer/ --force && cabal install Regex/ --force && cabal install Bnf/ --force && cabal install Parser/ --force && cabal install Loader/ --force
+
