3a19af2da9de8aa94a9ca47822569279cbc43fe6
pietervdvn <pietervdvn@gmail.com>
2014-10-16 00:51:13 +0200

    Added interperpreter to installall

--- installAll.sh
+++ installAll.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # installs all subcomponents, in the right order
-cabal install Std/ --force && cabal install Consumer/ --force && cabal install Regex/ --force && cabal install Bnf/ --force && cabal install Parser/ --force && cabal install Loader/ --force && cabal install SemanticAnalysis/ --force
+cabal install Std/ --force && cabal install Consumer/ --force && cabal install Regex/ --force && cabal install Bnf/ --force && cabal install Parser/ --force && cabal install Loader/ --force && cabal install SemanticAnalysis/ --force && cabal install Interpreter/ --force
 
