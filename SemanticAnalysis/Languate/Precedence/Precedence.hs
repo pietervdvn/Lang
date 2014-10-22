f1a609f40aaf9626c494da194945e15f246666d0
pietervdvn <pietervdvn@gmail.com>
2014-10-22 21:09:59 +0200

    Stub for precedence

---
+++ SemanticAnalysis/Languate/Precedence/Precedence.hs
@@ -0,0 +1,8 @@
+module Languate.Precedence.Precedence (PrecedenceTable, buildPrecTable, expr2prefExpr) where
+
+{--
+This module is an interface stub to export the important functions to the outside world
+--}
+
+import Languate.Precedence.PrecedenceTable
+import Languate.Precedence.Expr2PrefExpr
