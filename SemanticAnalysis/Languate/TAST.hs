c7a6f52102c9c215c4d9409e1eca7808e5a2ea72
pietervdvn <pietervdvn@gmail.com>
2014-11-24 16:37:54 +0100

    Moved Signature into TAST

--- SemanticAnalysis/Languate/TAST.hs
+++ SemanticAnalysis/Languate/TAST.hs
@@ -19,10 +19,20 @@ data Kind		= Kind Name
 			| KindCurry [Kind] Kind
 	deriving (Ord, Eq)
 
+-- resoved type -- each type known where it is defined
+data ResolvedType	= RNormal FQN Name
+			| RFree String
+			| RApplied RType [RType]
+			| RCurry [RType]
+			| RTuple RType [RType]
+			| RInfer
+type RType		= ResolvedType
+
+
 data TypedExpression	= TNat Int	| TFlt Float	| TChr Char	-- primitives
-			{- the first argument, [Type] are all the possible **return** types. E.g. '(&&) True False' -> Call [Bool] "&&" [..., ...]; '(&&) True' -> Call [Bool -> Bool] -}
-			| TApplication [Type] TypedExpression [TypedExpression]
-			| TCall [Type] Name
+			{- the first argument, [RType] are all the possible **return** types. E.g. '(&&) True False' -> Call [Bool] "&&" [..., ...]; '(&&) True' -> Call [Bool -> Bool] -}
+			| TApplication [RType] TypedExpression [TypedExpression]
+			| TCall [RType] Name
 	deriving (Show, Eq)
 type TExpression	= TypedExpression
 
@@ -35,6 +45,26 @@ data TPattern	= TAssign Name
 
 data TClause		= TClause [TPattern] TExpression
 	deriving (Show, Eq)
+
+data Signature	= Signature Name RType
+	deriving (Eq, Ord)
+
+
+-------------------- Only utils, instance declaration and boring stuff below -------------------------------------
+
+
+instance Show Signature where
+	show (Signature n t)
+	 	= tabs 2 n ++ ": "++ show t
+
+instance Normalizable Signature where
+	normalize (Signature n t)
+		= Signature n $ normalize t
+
+
+signature	:: Function -> [Signature]
+signature	=  map (uncurry Signature) . signs
+
 
 
 typeOf		:: TypedExpression -> [Type]
