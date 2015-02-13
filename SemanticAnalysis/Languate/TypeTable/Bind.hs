a038f0071b84f272b93c6b9c3c6f8af6eb88c42d
pietervdvn <pietervdvn@gmail.com>
2015-02-13 18:30:58 +0100

    Prepared testing of bind

--- SemanticAnalysis/Languate/TypeTable/Bind.hs
+++ SemanticAnalysis/Languate/TypeTable/Bind.hs
@@ -3,6 +3,5 @@ module Languate.TypeTable.Bind (module B) where
 {--
 Stub for bind code
 --}
-import Languate.TypeTable.Bind.Binding as B
 import Languate.TypeTable.Bind.Substitute as B
 import Languate.TypeTable.Bind.Bind as B
--- SemanticAnalysis/Languate/TypeTable/Bind/Bind.hs
+++ SemanticAnalysis/Languate/TypeTable/Bind/Bind.hs
@@ -35,13 +35,48 @@ Binds t0 in t1. If binding succeeds, this means t0 is a subtype (or equal type) 
 
 For each applied type, at most one supertypetable convert can happen.
 -}
-bind	:: TypeTable -> RType -> RType -> Either String Binding
+bind	:: TypeTable -> Map Name [RType] -> RType -> RType -> Either String Binding
 bind	= todo
 
 
 
 
 
+
+
+
+{- Tries to make two types the same, by filling in the frees in any of them.
+
+Unificate is associative.
+
+Type requirements and supertypes are **not** taken in account here.
+
+Used in "add binding", if conflicting values could be added
+
+-}
+unificate	:: RType -> RType -> Either String Binding
+unificate t0 t1
+	| t0 == t1	= return noBinding
+	| otherwise	= let 	ctx	= Context M.empty (error "No tt needed for unify!") noBinding
+				res	= runstateT (unificate' t0 t1) ctx in
+				res |> snd |> binding
+
+unificate'	:: RType -> RType -> StMsg ()
+unificate' (RFree a) (RFree b)
+	= do	addBinding (a, RFree b)
+		addBinding (b, RFree a)
+unificate' (RFree a) t1
+	= addBinding (a,t1)
+unificate' t0 (RFree b)
+	= addBinding (b,t0)
+unificate' (RCurry t0 t1) (RCurry t0' t1')
+	= do	unificate' t0 t0'
+		unificate' t1 t1'
+unificate' (RApplied bt at) (RApplied bt' at')
+	= do	unificate' bt bt'
+		unificate' at at'
+unificate' t0 t1
+	= assert (t0 == t1) $ "Could not unify "++ st True t0 ++" and "++ st True t1
 
 
 
