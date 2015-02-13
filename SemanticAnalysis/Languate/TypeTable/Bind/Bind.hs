3fc7805d910eb9d7019bea564ceaa6cb242516c7
pietervdvn <pietervdvn@gmail.com>
2015-02-13 01:06:14 +0100

    Removed unneeded unificate

--- SemanticAnalysis/Languate/TypeTable/Bind/Bind.hs
+++ SemanticAnalysis/Languate/TypeTable/Bind/Bind.hs
@@ -28,40 +28,6 @@ import Control.Monad.Trans
 
 import Debug.Trace
 
-{- Tries to make two types the same, by filling in the frees in any of them.
-
-Unificate is associative.
-
-Type requirements and supertypes are **not** taken in account here.
-
--}
-unificate	:: RType -> RType -> Either String Binding
-unificate t0 t1
-	| t0 == t1	= return noBinding
-	| otherwise	= let 	ctx	= Context M.empty (error "No tt needed for unify!") noBinding
-				res	= runstateT (unificate' t0 t1) ctx in
-				res |> snd |> binding
-
-unificate'	:: RType -> RType -> StMsg ()
-unificate' (RFree a) (RFree b)
-	= do	addBinding (a, RFree b)
-		addBinding (b, RFree a)
-unificate' (RFree a) t1
-	= addBinding (a,t1)
-unificate' t0 (RFree b)
-	= addBinding (b,t0)
-unificate' (RCurry t0 t1) (RCurry t0' t1')
-	= do	unificate' t0 t0'
-		unificate' t1 t1'
-unificate' (RApplied bt at) (RApplied bt' at')
-	= do	unificate' bt bt'
-		unificate' at at'
-unificate' t0 t1
-	= assert (t0 == t1) $ "Could not unify "++ st True t0 ++" and "++ st True t1
-
-
-
-
 
 
 
