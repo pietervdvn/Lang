d550c068fdc8601b48cf46e085f343fa7e42304c
pietervdvn <pietervdvn@gmail.com>
2015-02-14 18:51:18 +0100

    Added todo counter

--- searchTodo.sh
+++ searchTodo.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 # we drop the first two lines, as these are the todo in the stdDef
 find | grep "\.hs$" | grep -v "dist" | xargs grep "TODO" | tail -n +3
+echo -e "\nNumber of todos: "
+find | grep "\.hs$" | grep -v "dist" | xargs grep "TODO" | tail -n +3 | wc -l
