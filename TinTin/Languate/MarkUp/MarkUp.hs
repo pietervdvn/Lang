8aea986e1b3dff45328e0d2f6eb57ea4288bcdcd
Tleilaxu <Kenneth.VandenDriessche@ugent.be>
2015-03-05 21:47:21 +0100

    Markup rendering of parag, titling

--- TinTin/Languate/MarkUp/MarkUp.hs
+++ TinTin/Languate/MarkUp/MarkUp.hs
@@ -1,8 +1,10 @@
-module Languate.MarkUp.MarkUp (MarkUp (Base, Seq, Emph, Imp, Code, Incorr, Titling, Link), renderMD, renderHTML) where
+module Languate.MarkUp.MarkUp (MarkUp (Base, Parag, Seq, Emph, Imp, Code, Incorr, Titling, Link), renderMD, renderHTML) where
 
 -- This module implements the base definitions of the markup data structure
 
 import StdDef
+
+import State
 
 -- Represents a snippet of markUpped code
 data MarkUp
@@ -20,41 +22,55 @@ data MarkUp
 type MarkDown	= String
 type HTML	= String
 
-renderMD	:: MarkUp -> MarkDown
+renderMD	:: MarkUp -> State Int MarkDown
 renderMD (Base str)
-		= str
+		= return str
 renderMD (Parag mu)
-        = renderMD mu ++ "\n"
+        = return $ renderMD mu ++ "\n"
 renderMD (Seq mus)
-        = mus |> renderMD & unwords
+        = mus |> renderMD & unwords & return
 renderMD (Emph mu)
-		= renderMD mu & between "_"
+		= renderMD mu & between "_" & return
 renderMD (Imp mu)
-        = renderMD mu & between "**"
+        = renderMD mu & between "**" & return
 renderMD (Code mu)
-        = renderMD mu & between "```"
+        = renderMD mu & between "```" & return
 renderMD (Incorr mu)
-        = renderMD mu & between "~~"
+        = renderMD mu & between "~~" & return
+renderMD (Titling mu text)
+        = do i <- get
+             let title = replicate i "#" ++renderMD mu 
+             put $ i + 1
+             text' <- renderMD text
+             put i
+             return $ title ++ "\n\n"++text'
 renderMD (Link mu s)
-        = renderMD mu & between' "[" "]" ++ between' "(" ")" s
+        = renderMD mu & between' "[" "]" ++ between' "(" ")" s & return
 
-renderHTML	:: MarkUp -> HTML
+renderHTML	:: MarkUp -> State Int HTML
 renderHTML (Base str)
-		= str
+		= return str
 renderHTML (Parag mu)
-        = mu & renderHTML & inTag "p"
+        = return $ mu & renderHTML & inTag "p"
 renderHTML (Seq mus)
-        = mus |> renderHTML & unwords
+        = mus |> renderHTML & unwords & return
 renderHTML (Emph mu)
-		= mu & renderHTML & inDiv "emph"
+		= mu & renderHTML & inDiv "emph" & return 
 renderHTML (Imp mu)
-        = mu & renderHTML & inDiv "imp"
+        = mu & renderHTML & inDiv "imp" & return
 renderHTML (Code mu)
-        = mu & renderHTML & inDiv "code"
+        = mu & renderHTML & inDiv "code" & return 
 renderHTML (Incorr mu)
-        = mu & renderHTML & inDiv "incorr"
+        = mu & renderHTML & inDiv "incorr" & return 
+renderHTML (Titeling mu text)
+        = do i <- get
+            let title = renderHTML mu & inDiv ("title" ++ show i)
+            put $ i + 1
+            text' <- renderHTML text
+            put i
+            return $ title ++ text'
 renderHTML (Link text link)
-        = inTag' "a" ["href="++show link] $ renderHTML text
+        = inTag' "a" ["href="++show link] $ renderHTML text & return
 
 
 
