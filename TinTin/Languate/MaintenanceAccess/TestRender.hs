1e4ce94196ec6db750f65425cc177b9f08e6a40a
pietervdvn <pietervdvn@gmail.com>
2015-03-05 21:05:29 +0100

    Added testcode

--- TinTin/Languate/MaintenanceAccess/TestRender.hs
+++ TinTin/Languate/MaintenanceAccess/TestRender.hs
@@ -5,6 +5,16 @@ import StdDef
 import Languate.MarkUp.MarkUp
 t = do  putStrLn $ renderMD mu
         putStrLn $ renderHTML mu
- 
-mu = Seq [Link (Base "Abc") "Link"
-            , Emph (Base "Test")]
+
+mu = Seq    [ Base "Hallo"
+            , Emph (Base "Test")
+            , Imp "important"
+            , Code "x = \"code\""
+            , Incorr "wrong info"
+            , Titling "Main item" $ Seq
+			[ Parag "Information"
+			, Parag "More information"
+			, Titling "SubItem" "Hi"
+			, Titling "SubItem 2" "Hi again"]
+            , Link (Seq [Base "Some", Emph "link"]) "Link"
+            ]
