27a75dfa92000e0f5890aadc5c38437e87b5a380
Tleilaxu <Kenneth.VandenDriessche@ugent.be>
2015-03-10 23:15:54 +0100

    Merge and alignment

--- TinTin/Languate/MarkUp/MarkUp.hs
+++ TinTin/Languate/MarkUp/MarkUp.hs
@@ -9,16 +9,16 @@ import Data.Maybe
 import Data.List
 -- Represents a snippet of markUpped code
 data MarkUp
-        = Base String		    -- Embeds a plaintext in markup
-        | Parag MarkUp          -- Paragraph for markup
-        | Seq [MarkUp]          -- Sequence of markup
-	    | Emph MarkUp		    -- Emphasized markup
-        | Imp MarkUp 		    -- Important markup
-        | Code MarkUp 		    -- Code section
-        | Incorr MarkUp 	    -- Incorrect code
-        | Titling MarkUp MarkUp -- Embedded titeling [title, markup]
-        | Link MarkUp URL       -- A link with overlay text [markup, url]
-        | Table [MarkUp] [[MarkUp]]         -- A table [header, tablerows]
+        = Base String		            -- Embeds a plaintext in markup
+        | Parag MarkUp                  -- Paragraph for markup
+        | Seq [MarkUp]                  -- Sequence of markup
+        | Emph MarkUp		            -- Emphasized markup
+        | Imp MarkUp 		            -- Important markup
+        | Code MarkUp 		            -- Code section
+        | Incorr MarkUp 	            -- Incorrect code
+        | Titling MarkUp MarkUp         -- Embedded titeling [title, markup]
+        | Link MarkUp URL               -- A link with overlay text [markup, url]
+        | Table [MarkUp] [[MarkUp]]     -- A table [header, tablerows]
 
 type URL = String
 
@@ -80,10 +80,10 @@ renderMD (Link mu s)
 renderMD (Table mus muss)
         = do    header <- mapM renderMD mus
                 table  <- mapM (mapM renderMD) muss
-                let bars = intercalate " | "
+                let bars    = intercalate " | "
                 let header' = bars header
-                let lines = header ||>> const '-' & bars
-                let table' = table |> bars
+                let lines   = header ||>> const '-' & bars
+                let table'  = table |> bars
                 let content = "" : "" : header' : lines : table'
                 return $ unlines content
 
@@ -91,49 +91,49 @@ renderHTML	:: MarkUp -> State Int HTML
 renderHTML (Base str)
 		= return str
 renderHTML (Parag mu)
-        = mu & renderHTML |> inTag "p"
+        = mu & renderHTML       |> inTag "p"
 renderHTML (Seq mus)
-        = mus & mapM renderHTML |> unwords 
+        = mus & mapM renderHTML |> unwords
 renderHTML (Emph mu)
-		= mu & renderHTML |> inDiv "emph"
+		= mu & renderHTML       |> inDiv "emph"
 renderHTML (Imp mu)
-        = mu & renderHTML |> inDiv "imp"
+        = mu & renderHTML       |> inDiv "imp"
 renderHTML (Code mu)
-        = mu & renderHTML |> inDiv "code"
+        = mu & renderHTML       |> inDiv "code"
 renderHTML (Incorr mu)
-        = mu & renderHTML |> inDiv "incorr"
+        = mu & renderHTML       |> inDiv "incorr"
 renderHTML (Titling mu text)
-        = do i <- get
-             title <- renderHTML mu |> inDiv ("title" ++ show i)
-             put $ i + 1
-             text' <- renderHTML text
-             put i
-             return $ title ++ text'
+        = do    i <- get
+                title <- renderHTML mu |> inDiv ("title" ++ show i)
+                put $ i + 1
+                text' <- renderHTML text
+                put i
+                return $ title ++ text'
 renderHTML (Link text link)
         = renderHTML text |> inTag' "a" ["href="++show link]
 renderHTML (Table mus muss)
         = do    header <- mapM renderHTML mus
                 table  <- mapM (mapM renderHTML) muss
                 let header' = header |> inTag' "td" ["id=table_header"] & concat & inTag "tr"
-                let table' = table ||>> inTag "td" |> concat |> inTag "tr"
+                let table'  = table ||>> inTag "td" |> concat |> inTag "tr"
                 return $ inTag "table" $ inTag "tbody" $ concat $ header' : table'
 
 --------- TOOLS -------------
 
 between	:: String -> MarkDown -> MarkDown
-between str md = str++md++str
+between str md = str ++ md ++ str
 
 between':: String -> String -> String -> String
 between' start end str = start++str++end
 
 inTag   :: String -> HTML -> HTML
 inTag tagN html
-	= "<"++tagN++">"++html++"</"++tagN++">"
+	= "<" ++ tagN ++ ">" ++ html ++ "</" ++ tagN ++ ">"
 
 inTag'  :: String -> [String] -> HTML -> HTML
 inTag' tagN metas html
-    = "<"++tagN++" "++unwords metas ++">"++html++"</"++tagN++">"
+    = "<" ++ tagN ++ " " ++ unwords metas ++ ">" ++ html ++ "</" ++ tagN ++ ">"
 
 inDiv	:: String -> HTML -> HTML
-inDiv clas html
-	= "<div class=\""++clas++"\">"++html++"</div>"
+inDiv className html
+	= "<div class=\""i ++ className ++ "\">" ++ html ++ "</div>"
