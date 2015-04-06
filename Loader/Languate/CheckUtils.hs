5585ce9b50dc834f6e25e306fb6f77ac33d8208f
pietervdvn <pietervdvn@gmail.com>
2015-04-07 01:59:44 +0200

    Moved check utils

---
+++ Loader/Languate/CheckUtils.hs
@@ -0,0 +1,35 @@
+module Languate.CheckUtils where
+
+{--
+This module implements various recurring things in checks
+--}
+
+import Exceptions
+import Data.List
+
+
+type Check	= Exc ()
+type Exc a	= Exceptions' String a
+
+-- Checks wether each element in a list occurs exactly once
+unique ls	= length ls == length (nub ls)
+
+-- Checks wether all the elements in a list are the same
+allSame []	= True
+allSame (a:as)	= all (==a) as
+
+-- Checks wether or not a list contains exactly one element
+isSingleton [_]	= True
+isSingleton _	= False
+
+
+
+
+inFile fqn	=  inside $ "In the file "++show fqn
+onLine (line, col)
+		=  inside $ "On or below line "++show line
+
+onLocation (fqn, coor)
+		= inFile fqn . onLine coor
+
+inside str	=  stack' ((++) $ str ++ ":\n")
