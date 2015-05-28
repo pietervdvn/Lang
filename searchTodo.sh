9f93e093dc532a5114fe9e4c6e9f488e25880035
pietervdvn <pietervdvn@gmail.com>
2015-05-28 15:28:22 +0200

    Added pickup tracker

--- searchTodo.sh
+++ searchTodo.sh
@@ -3,3 +3,5 @@
 find | grep "\.hs$" | grep -v "dist" | xargs grep "TODO" | grep -v "StdDef.hs"
 echo -e "\nNumber of todos: "
 find | grep "\.hs$" | grep -v "dist" | xargs grep "TODO" | grep -v "StdDef.hs"| wc -l
+
+find | grep "\.hs$" | grep -v "dist" | xargs grep "PICKUP"
