2263cc64409136c8dfbb451842c07067a383071d
pietervdvn <pietervdvn@gmail.com>
2015-10-05 02:08:44 +0200

    Added renderstring for usage in command line environments

--- MarkUp/Languate/MarkUp.hs
+++ MarkUp/Languate/MarkUp.hs
@@ -1,4 +1,4 @@
-module Languate.MarkUp (module M) where
+module Languate.MarkUp (module M, renderString) where
 
 import Languate.MarkUp.MarkUp as M
 import Languate.MarkUp.Cluster as M
@@ -10,3 +10,21 @@ import Languate.MarkUp.MDOptions as M
 import Languate.MarkUp.Css as M
 
 -- Stub for reexporting markup
+
+import StdDef
+import Languate.MarkUp.MD
+import State
+
+
+renderString	:: MarkUp -> String
+renderString mu	=  let	md	= mu & rewrite _links	in
+			runstate (renderMD md) (MdContext 0 0) & fst
+
+_links		:: MarkUp -> Maybe MarkUp
+_links (Link mu _)
+		= Just mu
+_links (InLink mu _)
+		= Just mu
+_links (Embed nm)
+		= Just $ Base ("(see "++nm++")")
+_links _	= Nothing
