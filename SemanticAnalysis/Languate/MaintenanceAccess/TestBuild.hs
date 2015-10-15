ec50948d365f8cb853a1e3b6b60cef5a22fb8f54
pietervdvn <pietervdvn@gmail.com>
2015-10-15 18:59:57 +0200

    Big cleanup

--- SemanticAnalysis/Languate/MaintenanceAccess/TestBuild.hs
+++ SemanticAnalysis/Languate/MaintenanceAccess/TestBuild.hs
@@ -7,80 +7,40 @@ This module builds all the stuff!
 import qualified Bnf
 import StdDef
 import Exceptions
+import Languate.MarkUp
 
 
 import Languate.File2Package
 import Languate.FQN
 import Languate.AST
-import Languate.MarkUp as Mu
+import Languate.TAST
+import Languate.Typetable
+
+import Languate.Package
+import Languate.PackageTable
+
 
 import System.IO.Unsafe
 import System.Directory
 
+t	= packageTabl
 
-import Languate.TableOverview
-import Languate.BuildTableOverview
-
-import Languate.Package
-import Languate.TypeTable
-import Languate.TypeTable.BuildKnownTypes
-import Languate.TypeTable.BuildTypeLookupTable
-import Languate.TypeTable.KindChecker.BuildKindTable
-import Languate.TypeTable.BuildRequirementTable
-import Languate.TypeTable.BuildDocstringTable
-import Languate.TypeTable.BuildFreeNameTable
-import Languate.TypeTable.BuildSuperTT.BuildSuperTypeTable
-import Languate.TypeTable.BuildSuperTT.BuildSuperTypeTableFull
-import Languate.TypeTable.BuildSuperTT.FixImplicitRequirements
-import Languate.TypeTable.BuildSuperTT.ExpandFSTT
-
-import Languate.TypeTable.Checks.CheckPackage
-import Languate.TypeTable.Checks.CheckReqTable
-
-import Languate.CheckUtils
-
-import Data.Set as S
-
-import Data.Map
-import Data.Time.Clock
-import Data.Time.LocalTime
 
 bnfs		= unsafePerformIO $ Bnf.load "../Parser/bnf/Languate"
 path		= "../workspace/Data"
 packageIO	= loadPackage' bnfs
 
 loadedPackage	= unsafePerformIO $ packageIO path
-tablesOvervIO	= runExceptionsIO' $ buildAllTables loadedPackage
-tablesOverv	= unsafePerformIO tablesOvervIO
+packageTablIO	= runExceptionsIO' $ buildPackageTable loadedPackage
+packageTabl	= unsafePerformIO packageTablIO
 prelude		= toFQN' "pietervdvn:Data:Prelude"
 bool		= toFQN' "pietervdvn:Data:Data.Bool"
 
-
-bDocs	= do	dir	<- getCurrentDirectory
+bDocs	= do	dir		<- getCurrentDirectory
 		let cluster	= buildCluster []
 		package		<- packageIO path
-		tablesOverv	<- runExceptionsIO' $ buildAllTables package
-		let cluster'	= add tablesOverv cluster
-		addFooter'	<- addFooter
+		packT		<- runExceptionsIO' $ buildPackageTable package
+		let cluster'	= add packT cluster
 		let path'	= dir ++"/" ++ path ++ "/.gen" ++ "/html"
-		time	<- getCurrentTime |> utctDayTime |> realToFrac |> round
-		let hour = 2 + time `div` (60*60)
-		let css	= if hour `elem` ([0..8] ++ [21..24]) then blackCSS else defaultCSS
 		removeDirectoryRecursive path'
-		renderClusterTo (fix $ extend (setFilePath path' . addFooter' . addHeader) $ html $ defaultHeader css) cluster'
-
-
-links		:: MarkUp -> Maybe MarkUp
-links (InLink mu nm)
-		= Just $ InLink mu (nm ++ ".html")
-links _		= Nothing
-
-addFooter	:: IO (RenderSettings -> RenderSettings)
-addFooter	= do	let back = NonImp $ InLink (Base "Generated with Semantal - Back to all pages") "All pages"
-			time	<- getCurrentTime
-			print time
-			return $ addPreprocessor' (\mu -> Mu.Seq [back, mu,
-				NonImp $ parags [Base $ "This doc was automatically generated on "++show time, Base "Do not edit it, as changes will be erased the next generation.", back]])
-
-addHeader	:: RenderSettings -> RenderSettings
-addHeader	= addPreprocessor (\doc -> preprocess (\mu -> titling (title doc) $ Mu.Seq [notImportant $ description doc, contents doc ]) doc)
+		renderClusterTo (fix $ extend (setFilePath path') $ html $ defaultHeader defaultCSS) cluster'
