74707532236268be63006d7aee614af728c39d1a
pietervdvn <pietervdvn@gmail.com>
2015-11-13 02:52:59 +0100

    Added generated flag

--- SemanticAnalysis/Languate/FunctionTable/ModuleTraverser.hs
+++ SemanticAnalysis/Languate/FunctionTable/ModuleTraverser.hs
@@ -13,15 +13,19 @@ import Languate.AST
 import Languate.TAST
 import Languate.Typetable
 import Languate.FunctionTable.ConstructADTFunctions
+import Languate.FunctionTable.Def
+
+import Control.Arrow
 
 
 -- fetches the function signatures that are defined within the statement
-definedFuncSign	:: Module -> TypeLookupTable -> FQN -> Statement -> Exc [(Signature, Visible)]
+definedFuncSign	:: Module -> TypeLookupTable -> FQN -> Statement -> Exc [(Signature, (Visible, Generated))]
 definedFuncSign m tlt fqn (FunctionStm function)
 	= do	defs	<- signs function |+> resolveSignature tlt fqn
 		visibs	<- defs |+> getVisibility m (visibility function)
-		return (zip defs visibs)
+		let visibsGen	= zip visibs (repeat False)
+		return (zip defs visibsGen)
 definedFuncSign m tlt fqn (ADTDefStm adtDef)
-	= adtDefinedFunctions tlt fqn adtDef
+	= adtDefinedFunctions tlt fqn adtDef |||>>> (id &&& const True)
 definedFuncSign _ _ _ _
 	= return []
